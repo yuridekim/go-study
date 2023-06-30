aab47eca97a7b11fda9cca83c99743451055665b
yuridekim <cerisehill@korea.ac.kr>
2023-06-30 13:53:52 +0900

    Add Select

---
+++ select/v1/racer.go
@@ -0,0 +1,28 @@
+package racer
+
+import (
+	"net/http"
+	"time"
+)
+
+func Racer(a, b string) (winner string) {
+	startA := time.Now()
+	http.Get(a)
+	aDuration := time.Since(startA)
+
+	startB := time.Now()
+	http.Get(b)
+	bDuration := time.Since(startB)
+
+	if aDuration < bDuration {
+		return a
+	}
+
+	return b
+}
+
+func measureResponseTime(url string) time.Duration {
+	start := time.Now()
+	http.Get(url)
+	return time.Since(start)
+}
